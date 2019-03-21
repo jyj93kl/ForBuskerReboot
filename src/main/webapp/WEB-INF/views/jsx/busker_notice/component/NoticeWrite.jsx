548b4d7db55e973f85843f063d7279cb57c55203
Kim LeeJun <ljkim@misofamily.com>
2019-03-21 17:24:50 +0900

    공지사항 글쓰기 html editor 적용
    React Quill
    
    npm : npm install react-quill
    
    git tutorial : https://github.com/zenoamaro/react-quill

--- src/main/webapp/WEB-INF/views/jsx/busker_notice/component/NoticeWrite.jsx
+++ src/main/webapp/WEB-INF/views/jsx/busker_notice/component/NoticeWrite.jsx
@@ -2,20 +2,32 @@ import 'css/busker_common/reset.css';
 
 import React from 'react';
 
+import ReactQuill from 'react-quill'; // ES6
+import 'react-quill/dist/quill.snow.css';
+import 'css/busker_notice/notice_write.css';
+
 class NoticeWrite extends React.Component {
 
-    constructor(props) { 
-        super(props); 
-        
-        
+    constructor(props) {
+        super(props)
+        this.state = { text: '' } // You can also pass a Quill Delta here
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(value) {
+        this.setState({ text: value })
     }
 
     render() {
         return (
-            <div className="test">
-                notice write page
+            <div className="text-editor">
+                <input type="text"></input>
+                <ReactQuill 
+                    value={this.state.text} 
+                    onChange={this.handleChange}
+                />
             </div>
-        );
+        )
     }
 
 }
